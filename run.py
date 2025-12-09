9bdc11e71c2a0da2afea18fac2ed600e8e166f80
AndreyG <andreykasper@yandex.ru>
2025-12-10 01:45:11 +0400

    Switch Flask server for production

--- run.py
+++ run.py
@@ -25,4 +25,4 @@ def handle_exception(e):
 if __name__ == "__main__":
     logger.info("Starting Flask server...")
     # debug=True удобно для разработки; для продакшена выставить debug=False
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
